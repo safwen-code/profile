12013fee72111ac3803494367d69109d13b9de69
safwen-code <safwendjebbi1234@gmail.com>
2023-07-22 00:29:31 +0100

    v 0.1 of init radar for skills

--- src/components/content/Skillmain.jsx
+++ src/components/content/Skillmain.jsx
@@ -1,6 +1,38 @@
 import React from 'react'
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+import { Radar } from 'react-chartjs-2'
+
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+)
+
+export const data = {
+  labels: ['Thing 1', 'Thing 2', 'Thing 3', 'Thing 4', 'Thing 5', 'Thing 6'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [5, 9, 7, 5, 6, 3],
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1,
+    },
+  ],
+}
 
 const Skillmain = () => {
-  return <div>Exp and edu skills</div>
+  return <Radar data={data} />
 }
 export default Skillmain
