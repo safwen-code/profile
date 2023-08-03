733f81d7c386a13bd3d44aecd6b851989fe50f1c
safwen-code <safwendjebbi1234@gmail.com>
2023-08-03 16:29:25 +0100

    add contact

--- src/components/content/Contact.jsx
+++ src/components/content/Contact.jsx
@@ -6,8 +6,9 @@ const Contact = () => {
   return (
     <div className="container mt-5 ">
       <div className="row">
-        <Contactgeo />
-        <Contactvalidation />
+        contact
+        {/* <Contactgeo /> */}
+        {/* <Contactvalidation /> */}
         <div className="d-flex justify-content-center mt-4 mb-2 ">
           © 2023 All Rights Reserved by ib-themes.
         </div>
