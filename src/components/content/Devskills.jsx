0ba623a30dfd7948bce8a81598728f18722552c7
safwen-code <safwendjebbi1234@gmail.com>
2023-08-03 15:22:19 +0100

    v0.1 fro education and exp without popup things

--- src/components/content/Devskills.jsx
+++ src/components/content/Devskills.jsx
@@ -1,7 +1,176 @@
 import React from 'react'
+import {
+  Grid,
+  Card,
+  CardHeader,
+  Avatar,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  Divider,
+} from '@mui/material'
+import { deepOrange } from '@mui/material/colors'
 
 const Devskills = (props) => {
-  return <div>DevSkills</div>
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <div style={{ border: '1px solid #000000' }}>Education</div>
+          <Card sx={{ maxWidth: 345 }} style={{ marginBottom: '8px' }}>
+            <CardHeader
+              title="Go My Code"
+              subheader="October 2019, 2020"
+              avatar={
+                <Avatar sx={{ bgcolor: deepOrange[500] }} aria-label="recipe">
+                  GO
+                </Avatar>
+              }
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                Développer des application à l'aide du framework react for
+                frontend and nodejs for backend
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Typography
+                subtitle2="h6"
+                color="text.secondary"
+                style={{ fontSize: '0.9rem' }}
+                mr={3}
+              >
+                {' '}
+                Developpeur Full Stack
+              </Typography>
+              <Button size="small">Show more</Button>
+            </CardActions>
+          </Card>
+          <Card sx={{ maxWidth: 345 }} style={{ marginBottom: '8px' }}>
+            <CardHeader
+              title="Open Classroom"
+              subheader="October 2018, 2023"
+              avatar={
+                <Avatar sx={{ bgcolor: deepOrange[500] }} aria-label="recipe">
+                  OP
+                </Avatar>
+              }
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                Développer et construire des maquettes ansi contruire des api
+                with node ,php et c sharp
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Typography
+                subtitle2="h6"
+                color="text.secondary"
+                style={{ fontSize: '0.9rem' }}
+                mr={3}
+              >
+                {' '}
+                Developpeur
+              </Typography>
+              <Button size="small">Show more</Button>
+            </CardActions>
+          </Card>
+          <Card sx={{ maxWidth: 345 }} style={{ marginBottom: '8px' }}>
+            <CardHeader
+              title="ISET"
+              subheader="October 2013, 2017"
+              avatar={
+                <Avatar sx={{ bgcolor: deepOrange[500] }} aria-label="recipe">
+                  ISET
+                </Avatar>
+              }
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                Développer une application web pour la ressource pédagogique de
+                notre insitut
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Typography
+                subtitle2="h6"
+                color="text.secondary"
+                style={{ fontSize: '0.9rem' }}
+                mr={3}
+              >
+                {' '}
+                Technicien Supérieur En Développment
+              </Typography>
+              <Button size="small">Show more</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <div>Experience</div>
+          <Card sx={{ maxWidth: 345 }} style={{ marginBottom: '8px' }}>
+            <CardHeader
+              title="Bqubeits"
+              subheader="October 2021, 2023"
+              avatar={
+                <Avatar sx={{ bgcolor: deepOrange[500] }} aria-label="recipe">
+                  B
+                </Avatar>
+              }
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                Développer des application à l'aide du framework telque react ,
+                reactNative et jquery for industrie 4.0.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Typography
+                subtitle2="h6"
+                color="text.secondary"
+                style={{ fontSize: '0.9rem' }}
+                mr={3}
+              >
+                {' '}
+                Developpeur Full Stack
+              </Typography>
+              <Button size="small">Show more</Button>
+            </CardActions>
+          </Card>
+          <Card sx={{ maxWidth: 345 }} style={{ marginBottom: '8px' }}>
+            <CardHeader
+              title="Timelec"
+              subheader="October 2020, 2021"
+              avatar={
+                <Avatar sx={{ bgcolor: deepOrange[500] }} aria-label="recipe">
+                  Tim
+                </Avatar>
+              }
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                Montage du produit à l’aide du système FT bios et Controller le
+                produit.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Typography
+                subtitle2="h6"
+                color="text.secondary"
+                style={{ fontSize: '0.9rem' }}
+                mr={3}
+              >
+                {' '}
+                Operateur Du Montage
+              </Typography>
+              <Button size="small">Show more</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+  )
 }
 
 export default Devskills
