c8053ea39f167e24ae3db829c485fef3008294d4
safwen-code <safwendjebbi1234@gmail.com>
2023-08-04 17:59:08 +0100

    add email thing  v0.2

--- src/components/content/Contact.jsx
+++ src/components/content/Contact.jsx
@@ -1,21 +1,46 @@
 import React, { useState } from 'react'
-import { Divider, Grid, Typography, Button } from '@mui/material'
+import { Alert, Divider, Grid, Typography, Button } from '@mui/material'
 import SendIcon from '@mui/icons-material/Send'
-
 import TextField from '@mui/material/TextField'
+import emailjs from '@emailjs/browser'
+
 const Contact = () => {
   const [formdata, setformdata] = useState({
     name: '',
     email: '',
     message: '',
   })
+
+  const [alertVisible, setAlertVisible] = useState(false)
   const { name, email, message } = formdata
   const ChangeHundel = (e) => {
     setformdata({ ...formdata, [e.target.name]: e.target.value })
   }
-  const submitHundel = () => {
+  const submitHundel = (e) => {
+    e.preventDefault()
+    //console.log(Contact)
+    emailjs
+      .send(
+        'service_ctwp9zo',
+        'template_nf8js4z',
+        formdata,
+        'MCWeJugFmMWwVfeiC',
+      )
+      .then(
+        function (response) {
+          //console.log('SUCCESS!', response.status, response.text)
+          if (response.status === 200) {
+            setAlertVisible(true)
+            setTimeout(() => {
+              setAlertVisible(false)
+            }, 5000)
+          }
+        },
+        function (err) {
+          console.log('FAILED...', err)
+        },
+      )
     console.log(formdata)
-    console.log('hi')
   }
   return (
     <>
@@ -78,6 +103,18 @@ const Contact = () => {
       >
         Send
       </Button>
+      {alertVisible && (
+        <Alert
+          severity="success"
+          style={{
+            backgroundColor: '#f0f8ea',
+            color: '#4caf50',
+            fontWeight: 'bold',
+          }}
+        >
+          Thank you for your email! We'll get back to you soon.
+        </Alert>
+      )}
     </>
   )
 }
