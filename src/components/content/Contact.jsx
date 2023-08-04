735c811ebee66b5eb7c54ce274427ca382193195
safwen-code <safwendjebbi1234@gmail.com>
2023-08-04 15:58:01 +0100

    create a contact page v0.1

--- src/components/content/Contact.jsx
+++ src/components/content/Contact.jsx
@@ -1,19 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Divider, Grid, Typography, Button } from '@mui/material'
+import SendIcon from '@mui/icons-material/Send'
 
-import Contactgeo from './contentCantainers/Contactgeo'
-import Contactvalidation from './contentCantainers/Contactvalidation'
+import TextField from '@mui/material/TextField'
 const Contact = () => {
+  const [formdata, setformdata] = useState({
+    name: '',
+    email: '',
+    message: '',
+  })
+  const { name, email, message } = formdata
+  const ChangeHundel = (e) => {
+    setformdata({ ...formdata, [e.target.name]: e.target.value })
+  }
+  const submitHundel = () => {
+    console.log(formdata)
+    console.log('hi')
+  }
   return (
-    <div className="container mt-5 ">
-      <div className="row">
-        contact
-        {/* <Contactgeo /> */}
-        {/* <Contactvalidation /> */}
-        <div className="d-flex justify-content-center mt-4 mb-2 ">
-          © 2023 All Rights Reserved by ib-themes.
-        </div>
-      </div>
-    </div>
+    <>
+      <Typography align="center" variant="h5">
+        Contact Me
+      </Typography>{' '}
+      <Divider variant="middle" style={{ backgroundColor: '#4dabf5' }} mb={3} />
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            id="standard-read-only-input"
+            label="your name please"
+            variant="standard"
+            margin="dense"
+            name="name"
+            value={name}
+            onChange={ChangeHundel}
+            style={{ marginRight: '5px' }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            id="standard-read-only-input"
+            label="your email please"
+            variant="standard"
+            margin="dense"
+            name="email"
+            value={email}
+            onChange={ChangeHundel}
+          />{' '}
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          sx={{
+            width: 500,
+            maxWidth: '100%',
+            marginLeft: '10%',
+          }}
+        >
+          <TextField
+            fullWidth
+            label="message please"
+            id="fullWidth"
+            margin="dense"
+            multiline
+            rows={4}
+            name="message"
+            value={message}
+            onChange={ChangeHundel}
+          />
+        </Grid>
+      </Grid>
+      <Button
+        style={{ marginTop: '10px', marginBottom: '5px' }}
+        variant="outlined"
+        endIcon={<SendIcon />}
+        onClick={submitHundel}
+      >
+        Send
+      </Button>
+    </>
   )
 }
 
