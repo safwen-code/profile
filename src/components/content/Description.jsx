2026a7dc8c83a5f53f27f0789ed342f1c99875d2
safwen-code <safwendjebbi1234@gmail.com>
2023-08-08 23:01:19 +0100

    add filped card  v0.2

--- src/components/content/Description.jsx
+++ src/components/content/Description.jsx
@@ -10,6 +10,20 @@ import developpement from '../../images/development.png'
 import api from '../../images/api.jpg'
 import uimang from '../../images/uimang.png'
 
+import { Tilt } from 'react-tilt'
+
+const defaultOptions = {
+  reverse: false, // reverse the tilt direction
+  max: 35, // max tilt rotation (degrees)
+  perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+  scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
+  speed: 1000, // Speed of the enter/exit transition
+  transition: true, // Set a transition on enter/exit.
+  axis: null, // What axis should be disabled. Can be X or Y.
+  reset: true, // If the tilt effect has to be reset on exit.
+  easing: 'cubic-bezier(.03,.98,.52,.99)', // Easing on enter/exit.
+}
+
 const Description = () => {
   return (
     <>
@@ -29,84 +43,84 @@ const Description = () => {
       </Typography>
       <Divider light />
 
-      <Grid
-        container
-        // rowSpacing={1}
-        // columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-        spacing={2}
-        mt={1}
-      >
+      <Grid container spacing={2} mt={1}>
         <Grid item sm={6} md={4}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              alt="web dev"
-              height="100"
-              image={developpement}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Web Developpement
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                create apps in many domaine like education managment with react
-                jquery css sass and more utils.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">
-                More than 3 years experience in this domaine
-              </Button>
-            </CardActions>
-          </Card>
+          <Tilt options={defaultOptions}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="web dev"
+                height="100"
+                image={developpement}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  Web Developpement
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  create apps in many domaine like education managment with
+                  react jquery css sass and more utils.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">
+                  More than 3 years experience in this domaine
+                </Button>
+              </CardActions>
+            </Card>
+          </Tilt>
         </Grid>
         <Grid item sm={6} md={4}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              alt="backend dev"
-              height="100"
-              image={api}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                backend developpment
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                create Apis with node and php framewok using the concept of
-                micro-service systéme
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">
-                More of 2 years experience in this domaine "industry 4.7"
-              </Button>
-            </CardActions>
-          </Card>
+          <Tilt options={defaultOptions}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="backend dev"
+                height="100"
+                image={api}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  backend developpment
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  create Apis with node and php framewok using the concept of
+                  micro-service systéme
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">
+                  More of 2 years experience in this domaine "industry 4.7"
+                </Button>
+              </CardActions>
+            </Card>
+          </Tilt>
         </Grid>
         <Grid item sm={6} md={4}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              alt="Ui & Managment"
-              height="100"
-              image={uimang}
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                UI & Mangement
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                create ui concept interface depend the concept of project
-                management project with kanban srum and to do .
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">
-                More of 2 years experience in this domaine.
-              </Button>
-            </CardActions>
-          </Card>
+          <Tilt options={defaultOptions}>
+            <Card sx={{ maxWidth: 345 }}>
+              <CardMedia
+                component="img"
+                alt="Ui & Managment"
+                height="100"
+                image={uimang}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  UI & Mangement
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  create ui concept interface depend the concept of project
+                  management project with kanban srum and to do .
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">
+                  More of 2 years experience in this domaine.
+                </Button>
+              </CardActions>
+            </Card>
+          </Tilt>
         </Grid>
       </Grid>
     </>
